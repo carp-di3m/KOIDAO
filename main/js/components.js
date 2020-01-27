f9af7efbd5e7847291e888369c5cde59057d7250
carp-di3m <carp-di3m@protonmail.com>
2020-01-27 16:42:46 +0900

    refactored the bulma code A LOT via npm and lots of customization, added tab section and got more functionality

---
+++ main/js/components.js
@@ -0,0 +1,106 @@
+/*
+<div class="content"> 
+    <h2> Our Mission </h2>
+    <p> subtext</p>
+    <div class="card">
+        <h3>Advantages of Blockchain Gaming </h3>
+            <ul>
+                <li>one</li>
+                <li>one</li>
+                <li>one</li>
+            </ul>
+    </div>
+    <div class="card">
+        <h3>Problem</h3>
+            <ul>
+                <li>one</li>
+                <li>one</li>
+                <li>one</li>
+            </ul>
+    </div>
+    <div class="card">
+        <h3>Solution </h3>
+            <ul>
+                <li>one</li>
+                <li>one</li>
+                <li>one</li>
+            </ul>
+    </div>
+</div>
+*/
+
+
+function missionCreator() {
+    //creating all necessary elements for this section
+    
+    const content = document.createElement('div');
+    const title = document.createElement('h2');
+    const subtext = document.createElement('p');
+    const card1 = document.createElement('div');
+    const card1Title = document.createElement('h4');
+    const ul1 = document.createElement('ul');
+    const ul1Li1 = document.createElement('li');
+    const ul1Li2 = document.createElement('li');
+    const ul1Li3 = document.createElement('li');    
+    const card2 = document.createElement('div');
+    const card2Title = document.createElement('h4');
+    const ul2 = document.createElement('ul');
+    const ul2Li1 = document.createElement('li');
+    const ul2Li2 = document.createElement('li');
+    const ul2Li3 = document.createElement('li');
+    const card3 = document.createElement('div');
+    const card3Title = document.createElement('h4');
+    const ul3 = document.createElement('ul');
+    const ul3Li1 = document.createElement('li');
+    const ul3Li2 = document.createElement('li');
+    const ul3Li3 = document.createElement('li');
+
+    //add classes to elements
+    content.classList.add('content');
+    content.classList.add('is-medium');
+
+    //content for elements
+    title.textContent = 'Our Mission:'
+    subtext.textContent = 'blockchain gaming AI decentralization fixed-supply bonding curve mining governance interest dividends defi liquidity';
+    card1Title.textContent = 'Advantages of Using Blockchain for Gaming';
+    // card2Title.textContent = 'The Problem';
+    // card3Title.textContent = 'The Solution';
+    ul1Li1.textContent = 'filler one';
+    ul1Li2.textContent = 'filler two';
+    ul1Li3.textContent = 'filler three';
+    // ul2Li1.textContent = 'filler one';
+    // ul2Li2.textContent = 'filler two';
+    // ul2Li3.textContent = 'filler three';
+    // ul3Li1.textContent = 'filler one';
+    // ul3Li2.textContent = 'filler two';
+    // ul3Li3.textContent = 'filler three';
+
+    //appending elements to make tree-like structure
+    
+    content.appendChild(title);
+    content.appendChild(subtext);
+    content.appendChild(card1);
+    content.appendChild(card2);
+    content.appendChild(card3);
+    card1.appendChild(card1Title);
+    card2.appendChild(card2Title);
+    card3.appendChild(card3Title);
+    card1Title.appendChild(ul1);
+    card2Title.appendChild(ul2);
+    card3Title.appendChild(ul3);
+    ul1.appendChild(ul1Li1);
+    ul1.appendChild(ul1Li2);
+    ul1.appendChild(ul1Li3);
+    // ul2.appendChild(ul2Li1);
+    // ul2.appendChild(ul2Li2);
+    // ul2.appendChild(ul2Li3);
+    // ul3.appendChild(ul3Li1);
+    // ul3.appendChild(ul3Li2);
+    // ul3.appendChild(ul3Li3);
+
+    return content;
+
+};
+
+const tabContainer = document.querySelector('.tab-box');
+tabContainer.appendChild(missionCreator())
--- main/js/main.js
+++ main/js/main.js
@@ -1,4 +1,3 @@
-
 const yellowKoi = document.querySelector(".yellowKOI");
 const redKoi = document.querySelector(".redKOI");
 
@@ -8,45 +7,62 @@ const tl = new TimelineMax();
 // gsap.from(".redKOI", {duration: 2, x: 600})
 // gsap.from(".ethFRAME", {duration: 2, y: -600})
 
-tl.set('.yellowKOI', {
-    x: -600,
-    y: 30,
-    autoAlpha: 0,
-    rotation: 180
-})
-    .to('.yellowKOI', {
-        duration: 3,
-        x: 175,
-        autoAlpha: 0.95,
-        // ease: "power3.out",
-        rotation: 0
-    });
+tl.set(".yellowKOI", {
+  x: -600,
+  y: 30,
+  autoAlpha: 0,
+  rotation: 180
+}).to(".yellowKOI", {
+  duration: 3,
+  x: 175,
+  autoAlpha: 0.95,
+  // ease: "power3.out",
+  rotation: 0
+});
 const tl2 = new TimelineMax();
 
-    tl2.set('.redKOI', {
-        x: 600,
-        y: -30,
-        autoAlpha: 0,
-        rotation: -180
-    })
-        .to('.redKOI', {
-            duration: 3,
-            x: -175,
-            autoAlpha: 0.95,
-            // ease: "power3.out",
-            rotation: 0
-        });
+tl2
+  .set(".redKOI", {
+    x: 600,
+    y: -30,
+    autoAlpha: 0,
+    rotation: -180
+  })
+  .to(".redKOI", {
+    duration: 3,
+    x: -175,
+    autoAlpha: 0.95,
+    // ease: "power3.out",
+    rotation: 0
+  });
 
 const tl3 = new TimelineMax();
 
-    tl3.set(".ethFRAME", {
+tl3
+  .set(".ethFRAME", {
+    autoAlpha: -0.1
+  })
+  .to(".ethFRAME", {
+    duration: 10,
+    y: 0,
+    autoAlpha: 1,
+    // rotation: 360
+    ease: "back.out(1)"
+  });
+
+  $(document).ready(function() {
+
+    // Check for click events on the navbar burger icon
+    $(".navbar-burger").click(function() {
+  
+        // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+        $(".navbar-burger").toggleClass("is-active");
         
-        autoAlpha: -0.1
-    })
-        .to(".ethFRAME", {
-            duration: 10,
-            y: 0,
-            autoAlpha: 1,
-            // rotation: 360
-            ease: "back.out(1)"
-        })
+        
+        
+        $(".navbar-menu").toggleClass("is-active");
+        
+        
+  
+    });
+  });
